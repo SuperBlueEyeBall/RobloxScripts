1b7b32cb73b4a71fbcba2682be3fbd0495149fd3
Asi <148603975+SuperBlueEyeBall@users.noreply.github.com>
2024-07-05 15:33:09 -0500

    Create DisableChatLogger.lua

---
+++ DisableChatLogger.lua
@@ -0,0 +1,2 @@
+--Disable chat loggee script but with updates from the original. Feel free to just use the original, or use this one!
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AnthonyIsntHere/anthonysrepository/main/scripts/AntiChatLogger.lua", true))()
