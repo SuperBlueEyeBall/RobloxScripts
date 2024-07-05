0d189db87dc846e57653a9b67638748490a1ae8c
Asi <148603975+SuperBlueEyeBall@users.noreply.github.com>
2024-07-05 15:40:38 -0500

    Create ChatFilterBypass.lua

---
+++ ChatFilterBypass.lua
@@ -0,0 +1,2 @@
+--updatable version the Chat filter bypass. Feel free to use this one or the original script below.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Clickgameplay/no-tag-script/main/.NoTagScript", true))()
