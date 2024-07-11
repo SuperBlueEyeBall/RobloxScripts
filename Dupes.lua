85c0e061446c5747b720a94b5c73c7ae4bc8109f
Asi <148603975+SuperBlueEyeBall@users.noreply.github.com>
2024-07-10 21:04:05 -0500

    Update Dupes.lua

--- Dupes.lua
+++ Dupes.lua
@@ -1,2 +1,13 @@
---dupes is just a menu with alot of games supported such as admin rng. Go into the link in the loadstring to see the list of supported games as its being updated.
+--[[dupes is just a menu with alot of games supported such as admin rng. Go into the link in the loadstring to see the list of supported games as its being updated.
+currently supported games at the time of being updated:
+popItGame = 7346416636
+petsGame = 7903991471
+carsGame = 8216162783
+mvsdGame = 12355337193
+toysGame = 7503115095
+AdminrngGame = 17461365630
+mm2Game = 142823291
+DressTrdGame = 17893531862
+look at the id of the game if you don't know what game it is
+--]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Farx11122/Dupess/main/SecondDupe"))()
