7858236277150be1255b40c8d684fcb812c158ee
Asi <148603975+SuperBlueEyeBall@users.noreply.github.com>
2024-07-10 21:00:31 -0500

    Create Dupes.lua

---
+++ Dupes.lua
@@ -0,0 +1,2 @@
+--dupes is just a menu with alot of games supported such as admin rng. Go into the link in the loadstring to see the list of supported games as its being updated.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Farx11122/Dupess/main/SecondDupe"))()
