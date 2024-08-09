8be23bf848bd99a24a22f64fcfe2cf146c7b2d8d
Asi <148603975+SuperBlueEyeBall@users.noreply.github.com>
2024-08-09 14:33:10 -0500

    Update AdminRng.lua

--- AdminRng.lua
+++ AdminRng.lua
@@ -1,3 +1,4 @@
 getgenv().ValidateType = "Key" 
+-- I dont recommend using this one the key system is flippin HORRIBLE
 -- You can use "Whitelist" too! + pls don't remove nothing from this script or it can have errors. 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Prosexy/Demonic-HUB-V2/main/DemonicHub_V2.lua", true))()
